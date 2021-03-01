48294b3dfaed8774e5d9c4f1df16d5cb0312d035
breno-alv <breno.a01@gmail.com>
2021-03-01 15:27:38 -0300

    inserindo script

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+
+//Verificando se os campos nome é e-mail estão vazios.
+function validar() {
+    if (document.getElementById("nome").value ==""){
+        alert("Preencher o campo nome");
+        document.getElementById("nome").focus();
+        return false;
+    }
+
+    if (document.getElementById("email").value ==""){
+        alert("Preencher o campo email");
+        document.getElementById("email").focus();
+        return false;
+
+    } 
+
+}
+
+//bre um pop ups com uma mensagem 
 document.getElementById("button")
-.addEventListener("click", function () {alert("Sua mensagem foi enviada")});
+.addEventListener("click", function () {
+alert("Sua mensagem foi enviada")
+});
+
+
+
+
