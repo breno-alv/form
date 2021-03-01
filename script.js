0fe715bb00e27b6393e3c24e3784414dcb609c60
breno-alv <breno.a01@gmail.com>
2021-03-01 15:43:23 -0300

    pra tirar o erro

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+
 document.getElementById("button")
 .addEventListener("click", function () {alert("Sua mensagem foi enviada")});
+
+//document.addEventListener("click", function btn(e){
+    //const el = e.target;
+//})
+
+documet.location.reload(false);
