054ef43e354536a76da310eacd791ec8ee72b70e
breno-alv <breno.a01@gmail.com>
2021-03-17 17:01:22 -0300

    alterando o script

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 
-//Verificando se os campos nome é e-mail estão vazios.
-function validar() {
-    if (document.getElementById("nome").value ==""){
-        alert("Preencher o campo nome");
-        document.getElementById("nome").focus();
-        return false;
-    }
+// //Verificando se os campos nome é e-mail estão vazios.
+// function validar() {
+//     if (document.getElementById("nome").value ==""){
+//         alert("Preencher o campo nome");
+//         document.getElementById("nome").focus();
+//         return false;
+//     }
 
-    if (document.getElementById("email").value ==""){
-        alert("Preencher o campo email");
-        document.getElementById("email").focus();
-        return false;
+//     if (document.getElementById("email").value ==""){
+//         alert("Preencher o campo email");
+//         document.getElementById("email").focus();
+//         return false;
 
-    }
+//     }
     
-    if (document.getElementById("msg").value ==""){
-        alert("Escreva uma mensagem");
-        document.getElementById("msg").focus();
-        return false;
+//     if (document.getElementById("msg").value ==""){
+//         alert("Escreva uma mensagem");
+//         document.getElementById("msg").focus();
+//         return false;
 
-    } 
-}   
+//     } 
+// }   
 
-// document.getElementById("button")
-// .addEventListener("click", validar())
+// enviar => {
 //     alert("Mensagem enviada");
+// }
+
+document.getElementById("button")
+.addEventListener("click", enviar => alert("Mensagem enviada")); 
 
 
 
